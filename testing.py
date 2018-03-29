77664cf8c8516e9125105e45ee4a8b8b6998782b
raisonm <mattandthings@gmail.com>
2018-03-29 00:31:29 -0400

    test turtle file

---
+++ testing.py
@@ -0,0 +1,46 @@
+import turtle
+from mouse_interfaces import mouseClient
+
+
+mouse = mouseClient()
+
+def onDrag(x, y):
+    turtle.ondrag(None)
+    turtle.setposition(x,y)
+
+    mouse.setRelPosition(x - mouse.x, y - mouse.y)
+    mouse.setAbsolutePosition(x, y)
+    # I may need to multiply the y by -1 just to make sure that it converts well to the mouse control on the client.
+    mouse.y = y
+
+    turtle.ondrag(onDrag, 1, True)
+
+
+def onLeftClick(x,y):
+    print("L click");
+
+
+def onRightClick(x,y):
+    print("R click");
+
+
+def onRelease(x,y):
+    # print("Mouse absolute position = (%s, %s)" % (mouse.x, mouse.y))
+    # print("Mouse differential position = (%s, %s)" % (mouse.relX, mouse.relY))
+    turtle.setposition(0.0, 0.0)
+
+    mouse.x = 0
+    mouse.y = 0
+    mouse.relX = 0
+    mouse.relY = 0
+
+
+turtle.speed(10) # increase speed so turtle keeps up with mouse.
+turtle.shapesize(8, 8, 8)
+
+turtle.onclick(onLeftClick, 1, True)
+turtle.onclick(onRightClick, 2, True)
+turtle.ondrag(onDrag, 1, True)
+turtle.onrelease(onRelease, 1, True)
+
+turtle.done()
